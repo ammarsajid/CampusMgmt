e329513bea02e5150ed7aba65b693f28babe16c8
Ammar Sajid <ammar.sajid@techlogix.com>
2018-05-21 12:40:20 +0500

    test: to start CI build

--- CampusManagement/Controllers/StudentServicesController.cs
+++ CampusManagement/Controllers/StudentServicesController.cs
@@ -152,6 +152,7 @@ namespace CampusManagement.Controllers
             catch (SqlException sqle)
             {
                 return -2;
+                //to start continuous build
             }
         }
     }
